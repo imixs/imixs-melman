57e1ec1b6806bb7d09fcd2064791b0d8717835d0
Ralph Soika <ralph.soika@imixs.com>
2018-04-28 17:58:05 +0200

    implementation
    issue #3

---
+++ src/main/java/org/imixs/workflow/rest/JWTAuthenticator.java
@@ -0,0 +1,53 @@
+package org.imixs.workflow.rest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Logger;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+
+/**
+ * Client request Filter for Imixs-JWT 
+ * 
+ * @author rsoika
+ *
+ */
+public class JWTAuthenticator  implements ClientRequestFilter {
+
+    private final String jwt;
+	private final static Logger logger = Logger.getLogger(JWTAuthenticator.class.getName());
+
+
+    public JWTAuthenticator(String jwt) {
+        this.jwt = jwt;
+        }
+
+    public void filter(ClientRequestContext requestContext) throws IOException {
+    	
+    	URI uri = requestContext.getUri();
+    	
+    	String url=uri.toString();
+    	if (!url.contains("jwt=")) {
+    		logger.info("adding JSON Web Token...");
+    		if (url.contains("?")) {
+    			url+="&"+jwt;
+    		} else {
+    			url+="?"+jwt;
+    		}
+    		
+    		try {
+				requestContext.setUri(new URI(url));
+			} catch (URISyntaxException e) {
+				logger.severe("Failed to set JSON Web Token!");
+				e.printStackTrace();
+			}
+    		logger.info("update uri="+url);
+    		
+    	}
+    	
+    
+    }   
+
+}
