f7275bf7bef5245d84622160f5ffc070a9e9eda4
Ralph Soika <ralph.soika@imixs.com>
2018-04-28 17:55:28 +0200

    implementation
    issue #1

--- src/main/java/org/imixs/workflow/rest/WorkflowClient.java
+++ src/main/java/org/imixs/workflow/rest/WorkflowClient.java
@@ -30,64 +30,56 @@ import java.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.core.MediaType;
 
-//import org.glassfish.jersey.jackson.JacksonFeature;
 import org.imixs.workflow.ItemCollection;
 import org.imixs.workflow.xml.XMLDataCollection;
 import org.imixs.workflow.xml.XMLDocument;
 import org.imixs.workflow.xml.XMLDocumentAdapter;
- 
+
 /**
  * This ServiceClient is a WebService REST Client which encapsulate the
  * communication with a REST web serice based on the Imixs Workflow REST API.
- * The Implementation is based on the JAXB API.
- * 
- * The ServiceClient supports methods for posting EntityCollections and
- * XMLItemCollections.
- * 
- * The post method expects the rest service URI and a Dataobject based ont the
- * Imixs Workflow XML API
+ * The Implementation is based on the JAXB API 2.0.
  * 
  * 
- * http://www.adam-bien.com/roller/abien/entry/client_side_http_basic_access
- * 
- * 
- * 
- * @see org.imixs.workflow.jee.rest
  * @author Ralph Soika
  * 
  */
 public class WorkflowClient {
 
-	
-	
 	private final static Logger logger = Logger.getLogger(WorkflowClient.class.getName());
+
+	private Client client = null;
+	private String base_uri = null;
+
+	public WorkflowClient(String base_uri) {
+		super();
+
+		if (!base_uri.endsWith("/")) {
+			base_uri = base_uri + "/";
+		}
+		this.base_uri = base_uri;
+
+		logger.finest("......register jax-rs client for " + base_uri + "...");
+		client = ClientBuilder.newClient();
+	}
+
+	public void registerClientRequestFilter(ClientRequestFilter filter) {
+		logger.info("......register new request filter: " + filter.getClass().getSimpleName());
+		client.register(filter);
+	}
 
 	public ItemCollection getDocumentCustom(String uniqueid, String items) {
 
-		//Client client = ClientBuilder.newClient().register(JacksonFeature.class);
-		 
-		Client client = ClientBuilder.newClient();
-		 
-		client.register(new BasicAuthenticator("admin", "adminadmin"));
-		 
-
-		// Client client = ClientFactory.newClient();
-		// WebTarget target = client.target("http://example.com/shop");
-		// Form form = new Form().param("customer", "Bill")
-		// .param("product", "IPhone 5")
-		// .param("CC", "4444 4444 4444 4444");
-		//
-		// XMLDataCollection data=client.
-
-		XMLDataCollection data = client.target("http://localhost:8080/office-rest/documents/" + uniqueid)
-				.request(MediaType.APPLICATION_XML).get(XMLDataCollection.class);
+		XMLDataCollection data = client.target(base_uri + "documents/" + uniqueid).request(MediaType.APPLICATION_XML)
+				.get(XMLDataCollection.class);
 
 		if (data == null) {
 			return null;
 		} else {
-			if (data.getDocument().length==0) {
+			if (data.getDocument().length == 0) {
 				return null;
 			}
 			XMLDocument xmldoc = data.getDocument()[0];
@@ -95,22 +87,6 @@ public class WorkflowClient {
 			return XMLDocumentAdapter.putDocument(xmldoc);
 		}
 
-	//	return null;
 	}
-
-	/*
-	 * public ItemCollection getDocument(String uniqueid, String items) throws
-	 * MalformedURLException {
-	 * 
-	 * URL apiUrl = new URL("http://localhost:8080/office-rest"); DocumentService
-	 * documentService =
-	 * RestClientBuilder.newBuilder().baseUrl(apiUrl).build(DocumentService.class);
-	 * 
-	 * XMLDataCollection docs = documentService.getDocument(uniqueid, items); if
-	 * (docs == null) { return null; } else { XMLDocument xmldoc =
-	 * docs.getDocument()[0];
-	 * 
-	 * return XMLDocumentAdapter.putDocument(xmldoc); } }
-	 */
 
 }
