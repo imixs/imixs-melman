6736b56d76dadf7f637aad104f6f37304fb12c37
Ralph Soika <ralph.soika@imixs.com>
2019-09-05 18:34:46 +0200

    fixed
    Issue #24

--- src/main/java/org/imixs/melman/BasicAuthenticator.java
+++ src/main/java/org/imixs/melman/BasicAuthenticator.java
@@ -36,7 +36,7 @@ public class BasicAuthenticator  implements ClientRequestFilter {
     private String getBasicAuthentication() {
         String token = this.user + ":" + this.password;
         try {
-            return "BASIC " + DatatypeConverter.printBase64Binary(token.getBytes("UTF-8"));
+            return "Basic " + DatatypeConverter.printBase64Binary(token.getBytes("UTF-8"));
         } catch (UnsupportedEncodingException ex) {
             throw new IllegalStateException("Cannot encode with UTF-8", ex);
         }
