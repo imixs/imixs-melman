74347bdc38893df1a53d6d1eb9ed86f419003054
Ralph Soika <ralph.soika@imixs.com>
2021-05-31 12:26:46 +0200

    added new implementation
    Issue #46

---
+++ src/main/java/org/imixs/melman/CookieAuthenticator.java
@@ -0,0 +1,42 @@
+package org.imixs.melman;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.Cookie;
+
+/**
+ * Client request Filter for Cookies.
+ * <p>
+ * This ClientRequestFilter sets a cookie in each request
+ * 
+ * @author rsoika
+ *
+ */
+public class CookieAuthenticator implements ClientRequestFilter {
+
+	@SuppressWarnings("unused")
+	private final static Logger logger = Logger.getLogger(CookieAuthenticator.class.getName());
+
+	private Cookie cookie;
+
+	public CookieAuthenticator(Cookie cookie) {
+		super();
+		this.cookie = cookie;
+	}
+
+	/**
+	 * add cookie
+	 */
+	@Override
+	public void filter(ClientRequestContext clientRequestContext) {
+		List<Object> cookies = new ArrayList<>();
+		cookies.add(this.cookie);
+		clientRequestContext.getCookies().entrySet().stream().forEach(item -> cookies.add(item.getValue()));
+		clientRequestContext.getHeaders().put("Cookie", cookies);
+	}
+
+}
